8e905d08460cf73acaf60cda9fd6aef5b5c830f0
ashergrimmy <ashermambo@gmail.com>
2023-07-25 03:59:46 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,7 +7,8 @@
  * Return: Always 0 (success)
  */
 void reset_to_98(int *n)
-/* a function that takes a pointer to an int as parameter and updates the value it points to to 98. */	
+/* a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98. */	
 {
 	*n = 98;
 }
